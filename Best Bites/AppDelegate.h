917eb6c16d5f4be32cf1043f19a5b43c38f27f5d
Jeremy Klein <jeremyk23@yahoo.com>
2014-07-12 08:50:15 -0400

    Initial Commit

---
+++ Best Bites/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Best Bites
+//
+//  Created by Jeremy Klein Sr on 7/12/14.
+//  Copyright (c) 2014 Best Bites. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Best BitesTests/Best_BitesTests.m
@@ -0,0 +1,39 @@
+//
+//  Best_BitesTests.m
+//  Best BitesTests
+//
+//  Created by Jeremy Klein Sr on 7/12/14.
+//  Copyright (c) 2014 Best Bites. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Best_BitesTests : XCTestCase
+
+@end
+
+@implementation Best_BitesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
